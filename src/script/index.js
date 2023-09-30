92454c1589cd937c80514dd3e7adcf04e06aafd0
0xAco <adrien.couetoux.pro@gmail.com>
2023-09-30 21:49:30 +0200

    feat(js): clean pokemon object

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
 // get all pokemon URL
-async function getPokemonHub(){
+async function getPokemonHub() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000');
     if (await response.status !== 200) throw new Error(response.statusText);
@@ -9,14 +9,25 @@ async function getPokemonHub(){
   }
 }
 
-// get specific pokemon info by id
+// get specific pokemon info by id or name
 async function getPokemonById(id) {
-  // azy pepchite
-  // https://pokeapi.co/
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
+    if (await response.status !== 200) throw new Error(response.statusText);
+    return await response.json();
+  } catch (error) {
+    console.error('Erreur lors de la requête : ' + error.message);
+  }
 }
 
 // main code
 document.body.onload = async() => {
-  // const pokemonHub = await getPokemonHub();
-  console.log(await getPokemonById(700));
+  const sylveon = await getPokemonById('sylveon');
+  const keepProps = ['id', 'name', 'types'];
+  const sylveonClean = Object.fromEntries(
+    Object.entries(sylveon)
+      .filter(([key]) => keepProps.includes(key))
+  );
+  sylveonClean.types = sylveonClean.types.map(t => (t.type.name));
+  console.log(sylveonClean);
 };
