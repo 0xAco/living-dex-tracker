4f54041b03c1980b26e2b3dd117300d2ca7bab58
Adrien <91159803+0xAco@users.noreply.github.com>
2023-10-10 09:34:22 +0200

    Import data (#1)
    * azy cook un truc pepchite
    
    * feat(index.js) : add function getPkmn byID
    
    * feat(js): clean pokemon object
    
    * feat(data): import all dex until 9G - included
    
    ---------
    
    Co-authored-by: 0xAco <adrien.couetoux.pro@gmail.com>
    Co-authored-by: Camille Mallet <mallet.camille11@gmail.com>

---
+++ src/script/index.js
@@ -0,0 +1,34 @@
+// get all pokemon URL
+async function getPokemonHub() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=100000');
+    if (await response.status !== 200) throw new Error(response.statusText);
+    return await response.json();
+  } catch (error) {
+    console.error('Erreur lors de la requête : ' + error.message);
+  }
+}
+
+// get specific pokemon info by id or name
+async function getPokemonById(id) {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + id);
+    if (await response.status !== 200) throw new Error(response.statusText);
+    return await response.json();
+  } catch (error) {
+    console.error('Erreur lors de la requête : ' + error.message);
+  }
+}
+
+// main code
+document.body.onload = async() => {
+  const sylveon = await getPokemonById('sylveon');
+  
+  const keepProps = ['id', 'name', 'types'];
+  const sylveonClean = Object.fromEntries(
+    Object.entries(sylveon)
+      .filter(([key]) => keepProps.includes(key))
+  );
+  sylveonClean.types = sylveonClean.types.map(t => (t.type.name));
+  console.log(sylveonClean);
+};
